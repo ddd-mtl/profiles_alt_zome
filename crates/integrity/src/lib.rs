f8c12bf83cbd7de2f1e4caeb589be598c3f2a52c
damien <doute.d@gmail.com>
2024-03-19 14:56:42 -0400

    adding integrity

---
+++ crates/integrity/src/lib.rs
@@ -0,0 +1,39 @@
+//! ## hc_zome_profiles_integrity
+//!
+//! Profiles zome for any Holochain app.
+//!
+//! If you need to manage profiles (nickname, name, avatar, age and other useful personal information)
+//! you can directly include this zome and its coordinator counterpart [hc_zome_profiles_coordinator](https://docs.rs/hc_zome_profiles_coordinator) in your DNA.
+//!
+//! Read about how to include both this zome and its frontend module in your application [here](https://holochain-open-dev.github.io/profiles).
+
+use std::collections::BTreeMap;
+
+use hdi::prelude::*;
+
+/// Profile entry definition.
+///
+/// The profile must include at a minimum the nickname of the agent
+/// in order to be able to search for agents by nickname.
+#[hdk_entry_helper]
+#[derive(Clone)]
+pub struct Profile {
+    pub nickname: String,
+    pub fields: BTreeMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[hdk_entry_types]
+#[unit_enum(UnitEntryTypes)]
+pub enum EntryTypes {
+    Profile(Profile),
+}
+
+#[derive(Serialize, Deserialize)]
+#[hdk_link_types]
+pub enum LinkTypes {
+    PrefixPath,
+    PathToAgent,
+    AgentToProfile,
+}
