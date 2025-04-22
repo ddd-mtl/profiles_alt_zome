2dc7dfe1c6e04e2695e79d088ec8fcf27d73b463
damien <doute.d@gmail.com>
2025-04-22 15:45:17 -0400

    CapAccess::Unrestricted

--- crates/shared_ownership_coordinator/src/callbacks.rs
+++ crates/shared_ownership_coordinator/src/callbacks.rs
@@ -12,7 +12,7 @@ pub fn init(_: ()) -> ExternResult<InitCallbackResult> {
    fns.insert((zome_info()?.name, FunctionName("recv_shared_key".into())));
    let cap_grant_entry: CapGrantEntry = CapGrantEntry::new(
       String::from("remote signals"), // A string by which to later query for saved grants.
-      ().into(), // Unrestricted access means any external agent can call the extern
+      CapAccess::Unrestricted, // Unrestricted access means any external agent can call the extern
       GrantedFunctions::Listed(fns),
    );
    create_cap_grant(cap_grant_entry)?;
